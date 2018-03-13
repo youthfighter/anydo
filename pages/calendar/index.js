a1431b2bd819b495dc6e1944c9204bfbd9d8a462
youthfighter <youthfighter@163.com>
2018-03-13 23:20:00 +0800

    新建日历页面

---
+++ pages/calendar/index.js
@@ -0,0 +1,21 @@
+Page({
+  data: {
+    timeList:[
+      { yearmonth: '2018年3月',wek: '周一', day: '1'},
+      { yearmonth: '2018年3月', wek: '周二', day: '2' },
+      { yearmonth: '2018年3月', wek: '周三', day: '3' },
+      { yearmonth: '2018年3月', wek: '周四', day: '4' },
+      { yearmonth: '2018年3月', wek: '周五', day: '5' },
+      { yearmonth: '2018年3月', wek: '周六', day: '6' },
+      { yearmonth: '2018年3月', wek: '周日', day: '7' },
+      { yearmonth: '2018年3月', wek: '周一', day: '8' },
+      { yearmonth: '2018年3月', wek: '周二', day: '9' },
+      { yearmonth: '2018年3月', wek: '周三', day: '10' },
+      { yearmonth: '2018年3月', wek: '周四', day: '11' },
+      { yearmonth: '2018年3月', wek: '周五', day: '12' },
+      { yearmonth: '2018年3月', wek: '周六', day: '13' },
+      { yearmonth: '2018年3月', wek: '周日', day: '14' },
+    ],
+    cur:0
+  }
+})
